03d59c9d660a0a6e0b237398ba85e81b1931caff
Guillaume <gui.rouyer@gmail.com>
2025-10-14 10:26:20 +0200

    ReservationModel.php. L'erreur était due à une incohérence entre le nom du paramètre dans la requête SQL (:id_Locataire) et le nom du paramètre passé à la méthode execute (:id_proprietaire). J'ai mis à jour le code pour que les noms de paramètres correspondent

--- admin.sql
+++
@@ -1,8 +0,0 @@
-create table admin (
-    id_admin int primary key auto_increment,
-    nom_admin varchar(50) not null,
-    prenom_admin varchar(50) not null,
-    email_admin varchar(100) not null,
-    mot_de_passe_admin varchar(255) not null,
-    is_admin boolean default true
-);
